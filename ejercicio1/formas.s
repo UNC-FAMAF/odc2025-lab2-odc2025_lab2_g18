630a8a17a562d09bed76ef8709f555e782bd2640
Luca <31829436+Ifs26@users.noreply.github.com>
2025-05-29 19:14:10 -0300

    Actualizacion de algoritmos para generar figuras de cuatro lados. Ahora el rellenado actua similar a la herramienta de pintado de paint.

--- ejercicio1/formas.s
+++ ejercicio1/formas.s
@@ -10,9 +10,8 @@
     .global rectangulo
     .global pantalla
     .global circulo
-    .global cuadrado_b_relleno
     .global bresenham
-    .global bresenham_con_punto_fijo
+    .global cuadradoR
     .global pixel
 
 	//.global main
@@ -160,24 +159,15 @@ ret
 
 //CUADRILATERO
 //ANTES DE LLAMAR ASIGNAR LO QUE DICE EN CADA CONJUNTO DE INSTRUCCIONES
-cuadrado_b_relleno: 
-    /*Sean:
-    x9 = x1 //Mide que tan a la derecha debe ir
-    x10 = y1 //Mide que tanto debe bajar
-
-    x11 = x2
-    x12 = y2
-
-    x13 = x3
-    x14 = y3
-
-    x15 = x4
-    x16 = y4
-
-    x22 = punto_fijo_x
-    x23 = punto_fijo_y
-    */
-    sub sp, sp, #88
+cuadradoR:
+    /*Los registros usados son:
+        x9,x10 → x1,y1
+        x11,x12 → x2,y2
+        x13,x14 → x3,y3
+        x15,x16 → x4,y4
+        x22 → punto de partida del flood X
+        x23 → punto de partida del flood Y */
+    sub sp, sp, #96
     stur x9, [sp, #0]
     stur x10,[sp, #8]
     stur x11,[sp, #16]
@@ -189,38 +179,31 @@ cuadrado_b_relleno:
     stur x22,[sp, #64]
     stur x23,[sp, #72]
     stur x30,[sp, #80] //RE IMPORTANTE, ES EL LR (es PC+4)
-
-    //Primer lado
-    //Los puntos coinciden con los registros para el llamdo
-    mov x22, x13
-    mov x23, x14
-    bl bresenham_con_punto_fijo
+    //Primero trazo rectas
+    bl bresenham
     //segundo lado
     mov x9,x11
     mov x10,x12
     mov x11,x13
     mov x12,x14
-    mov x22, x15
-    mov x23, x16
-    bl bresenham_con_punto_fijo
+    bl bresenham
     //tercer lado
     mov x9,x13
     mov x10,x14
     mov x11,x15
     mov x12,x16
-    ldur x22,[sp,#0]
-    ldur x23,[sp,#8]
-    bl bresenham_con_punto_fijo
+    bl bresenham
     //cuarto lado
     mov x9,x15
     mov x10,x16
     ldur x11, [sp, #0]
     ldur x12, [sp, #8]
-    ldur x22,[sp,#16]
-    ldur x23,[sp,#24]
-    bl bresenham_con_punto_fijo
-    
-    
+    bl bresenham
+    //Ahora se rellena, recordar poner a x22,x23 dentro del cuadrado
+    bl flood_fill
+    add x22,x22,#1
+    bl flood_fill_der
+
     ldur x9, [sp, #0]
     ldur x10,[sp, #8]
     ldur x11,[sp, #16]
@@ -232,30 +215,21 @@ cuadrado_b_relleno:
     ldur x22,[sp, #64]
     ldur x23,[sp, #72]
     ldur x30,[sp, #80]
-    add sp, sp, #88
+    add sp, sp, #96
     ret
-
 
 bresenham:
     /* algoritmo de Bresenham 
-
         ¿Que hace?
             Traza rectas sin calcular la pendiente exacta.
-
         ¿Como lo hace?
-            Es un algoritmo voraz, por lo que su criterio de seleccion
-            para avanzar en x o y es la cantidad de error acumulado (dx-dy)
+            Es un algoritmo voraz, por lo que su criterio de seleccion para avanzar en x o y es la cantidad de error acumulado (dx-dy)
             (almacenado en x19).
-
-            El acumulador de error mide que tan desviado esta el paso actual
-            de la linea ideal. El algoritmo evalua las siguientes 
+            El acumulador de error mide que tan desviado esta el paso actual de la linea ideal. El algoritmo evalua las siguientes 
             condiciones para decidir en cada iteracion:
 
-                -> Si 2*x19 > -dy, lo cual significa que el error está más cerca 
-                de la siguiente columna, entonces se mueve en X.
-
-                -> Si 2*x19 < dx, lo cual significa que el error está más cerca 
-                de la siguiente fila, entonces se mueve en Y.
+                → Si 2*x19 > -dy, lo cual significa que el error está más cerca de la siguiente columna, entonces se mueve en X.
+                → Si 2*x19 < dx, lo cual significa que el error está más cerca de la siguiente fila, entonces se mueve en Y.
             
             Para mejor descripcion: https://saturncloud.io/blog/bresenham-line-algorithm-a-powerful-tool-for-efficient-line-drawing/?_gl=1*1e34fig*_gcl_au*MjEyMzM4MzgwOC4xNzQ3ODY3NjI0*_ga*MTE0MTAzMjE4NS4xNzQ3ODY3NjI1*_ga_9QKGCS5Q41*czE3NDc5NjE0MTgkbzIkZzAkdDE3NDc5NjE0MTgkajYwJGwwJGgwJGRzb1B4QktXb0RDc0NWQWlxRHpkbGhlTDlhWHFZUXhJcGVB#how-does-the-bresenham-line-algorithm-work
     */
@@ -266,26 +240,20 @@ bresenham:
         x4   → Direccion absoluta del píxel en memoria (base + offset)
         x5   → 2*err (doble del error actual, usado para criterio de selec)
         x6   → -dy (negativo del delta en Y, para comparaciones)
-        
         x9   → x0 (posicion actual en X)
         x10  → y0 (posicion actual en Y)
         x11  → x1 (posicion final en X)
         x12  → y1 (posicion final en Y)
-
         x13  → dx (|x1 - x0|, distancia en X)
         x14  → dy (|y1 - y0|, distancia en Y)
-
         x15  → sx (direccion en X: +1 si avanza, -1 si retrocede)
         x16  → auxiliar 
         x17  → sy (direccion en Y: +1 si sube, -1 si baja)
-
         x18  → auxiliar
         x19  → err (error acumulado, CRITERIO!)
-
         x21  → Color del pixel
     */
 
-    // Backup de registros alterados
     sub sp, sp, #136 //Se guardan cada 8, pero siempre debe quedar un multiplo de 16!
     stur x2,   [sp, #0]
     stur x3,   [sp, #8]
@@ -308,15 +276,13 @@ bresenham:
         // dx = abs(x1 - x0)
         subs x13, x11, x9
         bge abs_dx_done
-        neg x13, x13
+        sub x13, xzr, x13
     abs_dx_done:
-
         // dy = abs(y1 - y0)
         subs x14, x12, x10
         bge abs_dy_done
-        neg x14, x14
+        sub x14, xzr, x14
     abs_dy_done:
-
         // sx = (x0 < x1) ? 1 : -1
         cmp x9, x11
         mov x15, 1
@@ -324,7 +290,6 @@ bresenham:
         blt set_sx
         mov x15, x16
     set_sx:
-
         // sy = (y0 < y1) ? 1 : -1
         cmp x10, x12
         mov x17, 1
@@ -332,10 +297,8 @@ bresenham:
         blt set_sy
         mov x17, x16
     set_sy:
-
         // err = dx - dy
         sub x19, x13, x14
-
     bresenham_loop:
         // Direccion: (y0 * width + x0) * 4 + base
         mov x16, x9
@@ -353,7 +316,6 @@ bresenham:
         cmp x10, x12
         beq bresenham_restore
     check_y:
-
         lsl x5, x19, #1       //2*err
         neg x6, x14
         cmp x5, x6
@@ -361,15 +323,12 @@ bresenham:
         add x9, x9, x15
         sub x19, x19, x14
     skip_x:
-
         cmp x5, x13
         bge skip_y
         add x10, x10, x17
         add x19, x19, x13
     skip_y:
-
         b bresenham_loop
-
     bresenham_restore:
         // Restaurar registros
         ldur x2,   [sp, #0]
@@ -390,177 +349,124 @@ bresenham:
         ldur x19,  [sp, #120]
         ldur x30,  [sp, #128]
         add sp, sp, #136
-
     bresenham_end:
         ret
 
+flood_fill:
+    /*
+    IDEA: Se da un punto y un color, si el punto no es del color lo pinta y hace recursion hacia los puntos adyacentes. Si el punto es del color termina.
+    Es el metodo de pintado de paint (flood fill).
+    Los registros usados son:
+        x3 := Auxiliar
+        x4 := Guarda coordenada actual
+        x5 := Guarda color de coordenada actual 
+        x21 := color para rellenar  (No lo guardo en pila porque no se altera)
+        x22 = punto de partida del flood X
+        x23 = punto de partida del flood Y
+        x30 = LR */
+    sub sp, sp, #48
+    stur x3, [sp, #0]
+    stur x4,[sp, #8]
+    stur x5,[sp, #16]
+    stur x22,[sp, #24]
+    stur x23,[sp, #32]
+    stur x30,[sp, #40]
 
-
-bresenham_con_punto_fijo:
+    // if (x22 < 0 || x22 >= 640 || x23 < 0 || x23 >= 480) return;
+    //Esto verifica que no nos salimos de la pantalla
+    cmp x22, #0
+    blt no_rellenar
+    cmp x22, #640
+    bge no_rellenar
+    cmp x23, #0
+    blt no_rellenar
+    cmp x23, #480
+    bge no_rellenar
     
-    /* Registros utilizados:
+    // Tengo que implementar un if que compare el color pixel actual con el elegido
+    // Direccion: (y0 * width + x0) * 4 + base SE GUARDA EN X4
+    mov x3, #640
+    mul x3, x23, x3
+    add x3, x3, x22
+    lsl x3, x3, #2
+    add x4, x20, x3
+    //Comparacion con color
+    ldur w5, [x4]      // Cargo el color actual del pixel en w5
+    cmp w5, w21       // Comparo con el color objetivo
+    beq no_rellenar    // Si eran del mismo color
+    stur w21, [x4]      // Sino se rellena la ubicacion
+    
+    sub x22,x22,#1 //Izquierda
+    bl flood_fill
+    add x22,x22,#1
 
-        x3   → Direccion relativa del píxel a pintar (offset)
-        x4   → Direccion absoluta del píxel en memoria (base + offset)
-        x5   → 2*err (doble del error actual, usado para criterio de selec)
-        x6   → -dy (negativo del delta en Y, para comparaciones)
-        
-        x9   → x0 (posicion actual en X)
-        x10  → y0 (posicion actual en Y)
-        x11  → x1 (posicion final en X)
-        x12  → y1 (posicion final en Y)
+    add x23,x23,#1 //Arriba
+    bl flood_fill
+    sub x23,x23,#1
+    
+    sub x23,x23,#1 //Abajo
+    bl flood_fill
+    add x23,x23,#1 
+    
+    no_rellenar:
+    ldur x3, [sp, #0]
+    ldur x4,[sp, #8]
+    ldur x5,[sp, #16]
+    ldur x22,[sp, #24]
+    ldur x23,[sp, #32]
+    ldur x30,[sp, #40]
+    add sp, sp, #48
+    ret
 
-        x13  → dx (|x1 - x0|, distancia en X)
-        x14  → dy (|y1 - y0|, distancia en Y)
+flood_fill_der:
+    sub sp, sp, #48
+    stur x3, [sp, #0]
+    stur x4,[sp, #8]
+    stur x5,[sp, #16]
+    stur x22,[sp, #24]
+    stur x23,[sp, #32]
+    stur x30,[sp, #40]
 
-        x15  → sx (direccion en X: +1 si avanza, -1 si retrocede)
-        x16  → auxiliar 
-        x17  → sy (direccion en Y: +1 si sube, -1 si baja)
+    cmp x22, #0
+    blt no_rellenar_der
+    cmp x22, #640
+    bge no_rellenar_der
+    cmp x23, #0
+    blt no_rellenar_der
+    cmp x23, #480
+    bge no_rellenar_der
+    
+    mov x3, #640
+    mul x3, x23, x3
+    add x3, x3, x22
+    lsl x3, x3, #2
+    add x4, x20, x3
 
-        x18  → auxiliar
-        x19  → err (error acumulado, CRITERIO!)
+    ldur w5, [x4]      
+    cmp w5, w21       
+    beq no_rellenar_der    
+    stur w21, [x4]      
+    //Aca estan los cambios, ahora se va para la derecha en vez de la izquierda
+    add x22,x22,#1 //Derecha
+    bl flood_fill_der
+    sub x22,x22,#1
 
-        x21  → Color del pixel
-        
-        x22 -> Cordenada x a la que se ira todo el tiempo
-        x23 -> Cordenada y a la que se ira todo el tiempo
-    */
+    add x23,x23,#1 //Arriba
+    bl flood_fill_der
+    sub x23,x23,#1
 
-    // Backup de registros alterados
-    sub sp, sp, #152 //Se guardan cada 8 y se agrega 8 de mas!
-    stur x2,   [sp, #0]
-    stur x3,   [sp, #8]
-    stur x4,   [sp, #16]
-    stur x5,   [sp, #24]
-    stur x6,   [sp, #32]
-    stur x9,   [sp, #40]
-    stur x10,  [sp, #48]
-    stur x11,  [sp, #56]
-    stur x12,  [sp, #64]
-    stur x13,  [sp, #72]
-    stur x14,  [sp, #80]
-    stur x15,  [sp, #88]
-    stur x16,  [sp, #96]
-    stur x17,  [sp, #104]
-    stur x18,  [sp, #112]
-    stur x19,  [sp, #120]
-    stur x22,  [sp, #128]
-    stur x23,  [sp, #136]
-    stur x30,  [sp, #144]
-
-        // dx = abs(x1 - x0)
-        subs x13, x11, x9
-        bge abs_dx_done_bspj
-        neg x13, x13
-    abs_dx_done_bspj:
-
-        // dy = abs(y1 - y0)
-        subs x14, x12, x10
-        bge abs_dy_done_bspj
-        neg x14, x14
-    abs_dy_done_bspj:
-
-        // sx = (x0 < x1) ? 1 : -1
-        cmp x9, x11
-        mov x15, 1
-        mov x16, -1
-        blt set_sx_bspj
-        mov x15, x16
-    set_sx_bspj:
-
-        // sy = (y0 < y1) ? 1 : -1
-        cmp x10, x12
-        mov x17, 1
-        mov x16, -1
-        blt set_sy_bspj
-        mov x17, x16
-    set_sy_bspj:
-
-        // err = dx - dy
-        sub x19, x13, x14
-
-    bresenham_loop_bspj:
-        // Direccion: (y0 * width + x0) * 4 + base
-        mov x16, x9
-        mov x18, x10
-        mov x2, #640
-        mul x3, x18, x2
-        add x3, x3, x16
-        lsl x3, x3, #2
-        add x4, x20, x3
-        stur w21, [x4]
-
-        // -----------TRAZADO----------------------------
-        //Guardar registros actuales
-        // Guardar coordenadas actuales
-        //x16 y x18 eran auxiliares
-        mov x16, x9
-        mov x18, x10
-
-        // Preparar coordenadas para llamada a bresenham
-        mov x9, x16
-        mov x10, x18
-        mov x11, x22
-        mov x12, x23
-
-        bl bresenham
-
-        // Restaurar coordenadas originales para el loop
-        mov x9, x16
-        mov x10, x18
-        //mov x11,x22
-        ldur x11,[sp,#56]
-        ldur x12,[sp,#64]
-        //----------------------------------------------
-
-        // ¿Fin?
-        cmp x9, x11
-        bne check_y_bspj
-        cmp x10, x12
-        beq bresenham_restore_bspj
-    check_y_bspj:
-
-        lsl x5, x19, #1       //2*err
-        neg x6, x14
-        cmp x5, x6
-        ble skip_x_bspj
-        add x9, x9, x15
-        sub x19, x19, x14
-    skip_x_bspj:
-
-        cmp x5, x13
-        bge skip_y_bspj
-        add x10, x10, x17
-        add x19, x19, x13
-    skip_y_bspj:
-
-        b bresenham_loop_bspj
-
-    bresenham_restore_bspj:
-        // Restaurar registros
-        ldur x2,   [sp, #0]
-        ldur x3,   [sp, #8]
-        ldur x4,   [sp, #16]
-        ldur x5,   [sp, #24]
-        ldur x6,   [sp, #32]
-        ldur x9,   [sp, #40]
-        ldur x10,  [sp, #48]
-        ldur x11,  [sp, #56]
-        ldur x12,  [sp, #64]
-        ldur x13,  [sp, #72]
-        ldur x14,  [sp, #80]
-        ldur x15,  [sp, #88]
-        ldur x16,  [sp, #96]
-        ldur x17,  [sp, #104]
-        ldur x18,  [sp, #112]
-        ldur x19,  [sp, #120]
-        ldur x22,  [sp, #128]
-        ldur x23,  [sp, #136]
-        ldur x30,  [sp, #144]
-        
-        add sp, sp, #152
-
-    bresenham_end_bspj:
+    sub x23,x23,#1 //Abajo
+    bl flood_fill_der
+    add x23,x23,#1
+    
+    no_rellenar_der:
+    ldur x3, [sp, #0]
+    ldur x4,[sp, #8]
+    ldur x5,[sp, #16]
+    ldur x22,[sp, #24]
+    ldur x23,[sp, #32]
+    ldur x30,[sp, #40]
+    add sp, sp, #48
     ret
 
 //PIXEL
